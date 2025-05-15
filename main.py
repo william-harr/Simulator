2cf8fc5305c60d9a4191dad27ae165a23076105d
william-harr <wharr03@icloud.com>
2025-05-14 21:37:54 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Main entry point for the queueing system simulation package."""
+
+import sys
+from scripts.run_simulation import main
+
+if __name__ == '__main__':
+    main()
