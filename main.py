709ca57a61119c6fec47df6676861af7fb37b30b
william-harr <wharr03@icloud.com>
2025-05-14 21:42:59 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Main entry point for the queueing system simulation package."""
-
-import sys
-from scripts.run_simulation import main
-
-if __name__ == '__main__':
-    main()
